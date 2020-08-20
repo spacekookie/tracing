276244417cce6f9f3c3135b99565db5626f8ef88
Eliza Weisman <eliza@buoyant.io>
2020-08-20 11:38:41 -0700

    fix most of the macros
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- tracing/src/macros.rs
+++ tracing/src/macros.rs
@@ -1818,7 +1818,7 @@ macro_rules! level_enabled {
 macro_rules! valueset {
 
     // === base case ===
-    (@ { $(,)* $($val:expr),* $(,)* }, $next:expr $(,)*) => {
+    (@ { $(,)* $($val:expr),* $(,)* } $(,)*) => {
         &[ $($val),* ]
     };
 
@@ -1849,19 +1849,19 @@ macro_rules! valueset {
     };
     (@ { $(,)* $($out:expr),* }, $($k:ident).+, $($rest:tt)*) => {
         $crate::valueset!(
-            @ { $($out),*, $crate::field::Value::from(&$val) },
+            @ { $($out),*, $crate::field::Value::from(&$($k).+) },
             $($rest)*
         )
     };
     (@ { $(,)* $($out:expr),* }, ?$($k:ident).+, $($rest:tt)*) => {
         $crate::valueset!(
-            @ { $($out),*, $crate::field::Value::debug(&val) },
+            @ { $($out),*, $crate::field::Value::debug(&$($k).+) },
             $($rest)*
         )
     };
     (@ { $(,)* $($out:expr),* }, %$($k:ident).+, $($rest:tt)*) => {
         $crate::valueset!(
-            @ { $($out),*, $crate::field::Value::display(&$val) },
+            @ { $($out),*, $crate::field::Value::display(&$($k).+) },
             $($rest)*
         )
     };
@@ -1883,17 +1883,17 @@ macro_rules! valueset {
 
     (@ { $(,)* $($out:expr),* }, ?$($k:ident).+) => {
         $crate::valueset!(
-            @ { $($out),*, $crate::field::Value::debug(&$($k).+) },
+            @ { $($out),*, $crate::field::Value::debug(&$($k).+) }
         )
     };
     (@ { $(,)* $($out:expr),* }, %$($k:ident).+) => {
         $crate::valueset!(
-            @ { $($out),*, $crate::field::Value::display(&$($k).+) },
+            @ { $($out),*, $crate::field::Value::display(&$($k).+) }
         )
     };
     (@ { $(,)* $($out:expr),* }, $($k:ident).+) => {
         $crate::valueset!(
-            @ { $($out),*, $crate::field::Value::from(&$($k).+) },
+            @ { $($out),*, $crate::field::Value::from(&$($k).+) }
         )
     };
 
