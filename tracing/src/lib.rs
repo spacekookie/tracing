e4af6d893e906a3a39086a12e88c657e86c7de5a
Eliza Weisman <eliza@buoyant.io>
2020-09-04 15:55:07 -0700

    can't believe this might actually work
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- tracing/src/lib.rs
+++ tracing/src/lib.rs
@@ -896,6 +896,15 @@ pub mod __macro_support {
     use crate::stdlib::sync::atomic::{AtomicUsize, Ordering};
     use crate::{subscriber::Interest, Callsite, Metadata};
     use tracing_core::Once;
+    pub mod as_value {
+        pub use crate::field::convert::{
+            AsDebugAnyValue as _, AsDebugValue as _, AsDisplayAnyValue as _, AsDisplayValue as _,
+        };
+
+        pub use crate::stdlib::borrow::Borrow as _;
+    }
+
+    pub use crate::field::convert::Specialize;
 
     /// Callsite implementation used by macro-generated code.
     ///
